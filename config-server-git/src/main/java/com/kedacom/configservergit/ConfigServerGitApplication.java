061949506816157e1e63acadab006dc7d986cb72
suxiongwei <xwsu512@126.com>
2018-01-09 19:51:59 +0800

    配置中心

--- config-server-git/src/main/java/com/kedacom/configservergit/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/kedacom/configservergit/ConfigServerGitApplication.java
@@ -9,7 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ConfigServerGitApplication {
-
+	//启动项目后访问：http://localhost:1201/eureka-web/prod/master可查看到远程仓库配置文件信息
+	//参考博客；http://blog.didispace.com/spring-cloud-starter-dalston-3/
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerGitApplication.class, args);
 	}
